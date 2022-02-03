d17ad1f60e3da57ced296942c345f85a35c6cc6b
RJraj11 <98950237+RJraj11@users.noreply.github.com>
2022-02-03 17:30:27 +0530

    Update KiteZerodhaHomePage.java
    ss

--- kite-zerodha/src/main/java/pom_pacakge/KiteZerodhaHomePage.java
+++ kite-zerodha/src/main/java/pom_pacakge/KiteZerodhaHomePage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class KiteZerodhaHomePage {
 	WebDriver driver;
-	//changessse
+	//changesssew
 	@FindBy(xpath = "//span[text()='Dashboard']")
 	WebElement dashboard;
 	
